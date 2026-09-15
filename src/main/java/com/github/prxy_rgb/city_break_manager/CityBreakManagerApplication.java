32da4446dadc6b4a351a4f03c7799857c78ab0a0
aashi615 <aashigarg975@gmail.com>
2026-09-15 23:45:33 +0530

    refactor: clean up application entry point

--- src/main/java/com/github/prxy_rgb/city_break_manager/CityBreakManagerApplication.java
+++ src/main/java/com/github/prxy_rgb/city_break_manager/CityBreakManagerApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CityBreakManagerApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(CityBreakManagerApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityBreakManagerApplication.class, args);
+    }
 }
